ded703f427e3df88c344b0160b4194f40842806a
FranCast9 <francastellano9@gmail.com>
2024-05-21 12:49:38 +0100

    nuevo codigo

--- index.php
+++ index.php
@@ -13,21 +13,69 @@ $data = json_decode($result, true);
 
 curl_close($ch);
 
-var_dump($data);
 ?>
 
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Web que muestra la siguiente peliculal de marvel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La próxima pelicula de Marvel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/>
+</head>
+
 <main>
-    <h2>La proxima pelicula de Marvel</h2>
+    <div>
+        <h2>La próxima pelicula de Marvel</h2>
+    </div>
+
+    <div class="movie-info">
+        <h2><?= $data["title"] ?> se estrena en <?= $data["days_until"]?> dias</h2>
+        <img src="<?= $data["poster_url"] ?>" width= "300px";  alt="<?= $data["title"]?>">
+        <div class="info-row">
+            <p>Fecha de estreno: <?= $data["release_date"]?></p>
+            <p>Tipo de producción: <?= $data["type"] ?></p>
+        </div>
+    </div>
 </main>
 
 <style>
     :root {
-        color-scheme: light dark;
+        color-scheme: light dark;    
     }
 
-    body {
-        display: grid;
-        place-content: center;
+    main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+            padding: 20px;
+        }
+
+    .movie-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: center; 
+        align-items: center;     
+        height: 100%;         
+        width: 100%;
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+        margin: 20px;
+        border-radius: 20px;
+    }
+
+    .info-row {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        max-width: 400px;
+    }
+
+    P {
+        font-size: 14px;
     }
 
 </style>
